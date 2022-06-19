5811abb90b04a2a6f105a988723f1b795d236eb7
Александра <107440345+AlexsandraUKR@users.noreply.github.com>
2022-06-19 17:05:59 +0300

    Update and rename 01_19 Pasco_Alex.sql to ROWID.sql

--- ROWID.sql
+++ ROWID.sql
@@ -10,6 +10,7 @@ WHERE ST.ROWID IN 'AAASLYAAEAAAKoEAAK';
 DELETE FROM HR.STUDENTS_PAA ST
 WHERE ST.ROWID IN 'AAASLYAAEAAAKoEAAM';
 
+--На основе столбца rowid, написать  SQL запрос, который бы удалял (командой delete) 3 дублирующие записи.
 
 
 
